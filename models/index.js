b5362b2b53dce58a2a55eb25f13be5dc9ce1eceb
Timothy Allgood <timothyallgood@gmail.com>
2020-07-14 18:06:36 -0500

    using a mongdb atlas cluster, instead of local db

--- models/index.js
+++ models/index.js
@@ -1,19 +1,20 @@
 const mongoose = require('mongoose');
 const MONGODB_URI =
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/user-auth';
+	'mongodb+srv://timothy:yJ51plVHGzphoFGx@express-ecommerce.0mskm.mongodb.net/express-ecommerce?retryWrites=true&w=majority' ||
+	'mongodb://localhost:27017/user-auth';
 
 mongoose
-  .connect(MONGODB_URI, {
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log('Mongoose successfully connected'))
-  .catch((err) => console.log(err));
+	.connect(MONGODB_URI, {
+		useCreateIndex: true,
+		useFindAndModify: false,
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => console.log('Mongoose successfully connected'))
+	.catch((err) => console.log(err));
 
 module.exports = {
-  User: require('./User'),
-  Products: require(`./Products`),
-  productsArr: require('./productsArr'),
+	User: require('./User'),
+	Products: require(`./Products`),
+	productsArr: require('./productsArr'),
 };
