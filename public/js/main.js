a5ddee191c23e5b3d0b8e2dacd427b885ffa9f9c
Timothy Allgood <timothyallgood@gmail.com>
2020-07-13 19:24:03 -0500

    cleaned up javscript

--- public/js/main.js
+++ public/js/main.js
@@ -1,25 +1,29 @@
+// Variables for Review Form
 const rating = document.querySelector('.rating-container');
 const inputs = document.querySelectorAll('.rating-container input');
 const stars = document.querySelectorAll('.rating-container [type="radio"] + i');
 
+// Variables for Individual Reviews
 const reviews = document.querySelectorAll('.review');
 const ratingStr = document.querySelectorAll('.rating');
 const reviewStarsContain = document.querySelectorAll('.review .review-rating');
 
+// Amount of stars equal to rating in db are updated with proper styling
 for (let i = 0; i < reviewStarsContain.length; i++) {
 	const reviewStars = reviewStarsContain[i].querySelectorAll(`i`);
 	let ratingNum = parseInt(ratingStr[i].id);
 	console.log(ratingNum);
 	for (let j = 0; j < reviewStars.length; j++) {
 		if (j < ratingNum) {
-			reviewStars[j].classList.remove('far');
-			reviewStars[j].classList.add('fas');
-			reviewStars[j].style.color = 'goldenrod';
+			changeStar(reviewStars[j], 'far', 'fas', 'goldenrod');
 		}
 	}
 }
 
-rating.addEventListener('click', (e) => {
+// Changes colors of amount of stars clicked
+rating.addEventListener('click', handleStarUpdate);
+
+function handleStarUpdate(e) {
 	let value = null;
 	if (e.target.type === 'radio') {
 		value = e.target.value;
@@ -27,16 +31,16 @@ rating.addEventListener('click', (e) => {
 
 	if (value) {
 		for (i = 0; i < inputs.length; i++) {
-			stars[i].style.color = 'initial';
-			stars[i].classList.remove('fas');
-			stars[i].classList.add('far');
+			changeStar(stars[i], 'fas', 'far', 'initial');
 			if (inputs[i].value <= value) {
-				stars[i].classList.remove('far');
-				stars[i].classList.add('fas');
-				stars[i].style.color = 'goldenrod';
+				changeStar(stars[i], 'far', 'fas', 'goldenrod');
 			}
 		}
 	}
-
-	console.log(value);
-});
+}
+// Changes Star Color and Updates Icon
+function changeStar(star, oldIcon, newIcon, color) {
+	star.classList.remove(oldIcon);
+	star.classList.add(newIcon);
+	star.style.color = color;
+}
